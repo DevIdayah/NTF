ee8ffad858095d3c06d7e9349031c6fcf662307f
DevIdayah <97677077+DevIdayah@users.noreply.github.com>
2023-01-20 11:45:51 +0100

    my first commit on NTF

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        hidayat: "#0D1A2D",
+        farhan: "#15273F",
+        primary: "#6F8CB4",
+        bprimary: "#02EEEA"
+      }
+    },
+  },
+  plugins: [],
+}
+
